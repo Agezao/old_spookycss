e61047e8d7c7eebc92337b3539ccc893e9d74816
agezao <0800.grc@gmail.com>
2016-12-25 21:48:50 -0200

    Initial commit, changing stuff to gulp and building gulp tasks + Tropicalization of the assets

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+'use strict';
+ 
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('sass:prod', function () {
+  return gulp.src('./scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'))
+    .pipe(sourcemaps.init())
+    .pipe(cleanCSS())
+    .pipe(sourcemaps.write())
+    .pipe(rename('spooky.min.css'))
+    .pipe(gulp.dest('./css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./scss/**/*.scss', ['sass']);
+});
+
+///////
+
+gulp.task('default', ['sass']);
+
+gulp.task('build', ['sass:prod']);
+
+gulp.task('dev', ['sass:watch']);
